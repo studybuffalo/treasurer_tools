2fa4ba32413175dcb87229df4103b26d24d7d617
studybuffalo <studybuffalo@gmail.com>
2017-11-14 21:48:57 -0700

    Adding a placeholder home page

---
+++ urls.py
@@ -0,0 +1,9 @@
+"""Main Site URL Conf"""
+from django.conf.urls import url
+from django.contrib import admin
+from django.views.generic import TemplateView
+
+urlpatterns = [
+    url(r'^admin/', admin.site.urls),
+    url(r"^$", TemplateView.as_view(template_name="main/index.html")),
+]
