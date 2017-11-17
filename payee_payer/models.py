0351a32dae31f4ccc4dd91231f8ea14ba655b3f4
studybuffalo <studybuffalo@gmail.com>
2017-11-16 21:49:49 -0700

    Adding an optional user link (future use for whenever the user system is decided upon)

--- payee_payer/models.py
+++ payee_payer/models.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 from simple_history.models import HistoricalRecords
@@ -6,6 +7,12 @@ from payee_payer.services import get_country_list
 
 class Demographics(models.Model):
     """Demographic details for payees/payers"""
+    user = models.OneToOneField(
+        User,
+        unique=True,
+        blank=True,
+        null=True,
+    )
     name = models.CharField(
         max_length=256,
     )
@@ -29,7 +36,9 @@ class Demographics(models.Model):
         max_length=20,
     )
     fax = models.CharField(
+        blank=True,
         max_length=20,
+        null=True,
     )
     email = models.EmailField()
     status = models.CharField(
