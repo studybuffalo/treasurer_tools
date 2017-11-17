d807badd16e54b62a7228ae72f5e5b5f9c4362a4
studybuffalo <studybuffalo@gmail.com>
2017-11-16 19:39:04 -0700

    Adding the simple-history library, populating an initial requirements.txt

--- payee_payer/models.py
+++ payee_payer/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
-# Create your models here.
+from simple_history.models import HistoricalRecords
+
+class Demographics(models.Model):
+    """Demographic details for payees/payers"""
+    name = models.CharField()
+    address = models.CharField()
+    city = models.CharField()
+    province = models.CharField()
+    country = models.CharField()
+    postal_code = models.CharField()
+    phone = models.CharField()
+    fax = models.CharField()
+    email = models.EmailField()
+    status = models.CharField()
+    user_modified = models.ForeignKey()
+    date_modified = models.DateTimeField()
+    history = HistoricalRecords()
+    class Meta:
+        None
+
+    def __str__(self):
+        return ""
--- settings.py
+++ settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "simple_history",
 ]
 
 MIDDLEWARE = [
@@ -48,6 +49,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "simple_history.middleware.HistoryRequestMiddleware",
 ]
 
 ROOT_URLCONF = "urls"
