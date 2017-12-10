9752ddbc3bd5f4a83a29e685bf5149cb53fec27f
Joshua Torrance <torrancj@gmail.com>
2017-12-10 14:53:28 -0700

    Construction of investment models

--- investments/models.py
+++ investments/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
-# Create your models here.
+from simple_history.models import HistoricalRecords
+
+
+class Investment(models.Model):
+    """Records data on financial invetments"""
+    name = models.CharField(
+        help_text="Name to identify the investment",
+        max_length=256,
+    )
+    date_invested = models.CharField(
+        help_text="Date of initial investment",
+    )
+    amount = models.DecimalField(
+        decimal_places=2,
+        help_text="Initial amount invested",
+        max_digits=12,
+    )
+    rate = models.CharField(
+        help_text="Details on rate, term duration, etc.",
+        max_length=256,
+    )
+    history = HistoricalRecords()
