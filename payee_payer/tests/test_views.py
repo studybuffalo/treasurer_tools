8018d5b4bec0a7eb7304ad63f6f636c2ecb25dda
Joshua Torrance <torrancj@gmail.com>
2017-12-28 13:45:28 -0700

    Adding tests to confirm the list retrieval is correct

--- payee_payer/tests/test_views.py
+++ payee_payer/tests/test_views.py
@@ -3,9 +3,9 @@
 from django.core.urlresolvers import reverse
 from django.test import TestCase
 
-from payee_payer.models import Country
+from payee_payer.models import Country, Demographics
 
-class CountryModelTest(TestCase):
+class DashboardTest(TestCase):
     """Test functions for the Demographics model"""
     # pylint: disable=no-member,protected-access
 
@@ -15,23 +15,82 @@ class CountryModelTest(TestCase):
         "payee_payer/tests/fixtures/demographics.json",
     ]
 
+    def test_dashboard_redirect_if_not_logged_in(self):
+        """Checks redirect to login page if user is not logged in"""
+        response = self.client.get("/payee-payer/")
+
+        self.assertRedirects(response, "/accounts/login/?next=/payee-payer/")
+
     def test_dashboard_url_exists_at_desired_location(self):
         """Checks that the dashboard uses the correct URL"""
         login = self.client.login(username="user", password="abcd123456")
         response = self.client.get("/payee-payer/")
+        
+        # Check that user logged in
+        self.assertEqual(str(response.context['user']), 'user')
 
+        # Check that page is accessible
         self.assertEqual(response.status_code, 200)
 
     def test_dashboard_accessible_by_name(self):
         """Checks that the dashboard URL name works properly"""
         login = self.client.login(username="user", password="abcd123456")
         response = self.client.get(reverse("payee_payer_dashboard"))
+        
+        # Check that user logged in
+        self.assertEqual(str(response.context['user']), 'user')
 
+        # Check that page is accessible
         self.assertEqual(response.status_code, 200)
 
     def test_dashboard_template(self):
         """Checks that the dashboard uses the correct template"""
         login = self.client.login(username="user", password="abcd123456")
         response = self.client.get(reverse("payee_payer_dashboard"))
+        
+        # Check that user logged in
+        self.assertEqual(str(response.context['user']), 'user')
 
+        # Check for proper template
         self.assertTemplateUsed(response, "payee_payer/index.html")
+
+class RetrieveListTest(TestCase):
+    """Test functions for the Demographics model"""
+    # pylint: disable=no-member,protected-access
+
+    fixtures = [
+        "payee_payer/tests/fixtures/authentication.json",
+        "payee_payer/tests/fixtures/country.json",
+        "payee_payer/tests/fixtures/demographics.json",
+    ]
+
+    def test_retrieve_list_redirect_if_not_logged_in(self):
+        """Checks that request is redirected if user is not logged in"""
+        response = self.client.get("/payee-payer/retrieve-payee-payer-list/")
+
+        self.assertEqual(response.status_code, 302)
+
+    def test_retrieve_list_url_exists_at_desired_location(self):
+        """Checks that the retrieve payee/payer list URL is correct"""
+        login = self.client.login(username="user", password="abcd123456")
+        response = self.client.get("/payee-payer/retrieve-payee-payer-list/")
+
+        # Check that user logged in
+        self.assertEqual(str(response.context['user']), 'user')
+
+        # Check that response is corret
+        self.assertEqual(response.status_code, 200)
+
+    def test_retrieve_list_all_entries(self):
+        """Checks that all payee_payer entries are retrieved"""
+        login = self.client.login(username="user", password="abcd123456")
+        response = self.client.get("/payee-payer/retrieve-payee-payer-list/")
+        
+        # Check that user logged in
+        self.assertEqual(str(response.context['user']), 'user')
+
+        # Check that the number of retrieved entries matches the DB
+        db_count = Demographics.objects.all().count
+        context_count = len(response.context['payee_payer_list'])
+
+        self.assertTrue(True)
