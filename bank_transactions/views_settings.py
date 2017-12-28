bbe0179d62cca262b41b2940598a5eef8e8fe748
studybuffalo <studybuffalo@gmail.com>
2017-12-27 18:17:00 -0700

    Adding functionality to edit institutions and accounts

--- bank_transactions/views_settings.py
+++ bank_transactions/views_settings.py
@@ -83,7 +83,7 @@ def institution_add(request):
 
         # Check if the form is valid:
         if form.is_valid():
-            # Create a item form instance and provide it the transaction object
+            # Create a item form instance and provide it the institution object
             formsets = account_formset(
                 request.POST, instance=institution_data
             )
@@ -119,66 +119,63 @@ def institution_edit(request, institution_id):
     """Generate and processes form to edit a financial system"""
 
     def update_transaction_form(form):
-        """Updates transaction based on the provided form"""
-
+        """Updates Institution instance based on provided form data"""
         # Collect the cleaned form fields
-        payee_payer = form.cleaned_data["payee_payer"]
-        memo = form.cleaned_data["memo"]
-        date_submitted = form.cleaned_data["date_submitted"]
-        transaction_type = "e" if t_type == "expense" else "r"
+        name = form.cleaned_data["name"]
+        address = form.cleaned_data["address"]
+        phone = form.cleaned_data["phone"]
+        fax = form.cleaned_data["fax"]
 
         # Set the model data and save the instance
-        transaction_data.payee_payer = payee_payer
-        transaction_data.memo = memo
-        transaction_data.date_submitted = date_submitted
-        transaction_data.transaction_type = transaction_type
+        institution_data.name = name
+        institution_data.address = address
+        institution_data.phone = phone
+        institution_data.fax = fax
 
-        transaction_data.save()
+        institution_data.save()
 
-    def update_item_formset(formset):
-        """Create/updates an Item based on the provided formset"""
+    
+    def update_item_formset(formset, institution_data):
+        """Create/updates BankTransaction based on provided formset data"""
         # Only save non-empty forms
         if formset.cleaned_data:
-            # Check if this item is marked for deletion
+             # Check if this item is marked for deletion
             can_delete = formset.cleaned_data["DELETE"]
 
-            # Get this item ID
+            # Get this account ID
             if formset.cleaned_data["id"]:
-                item_exists = True
+                account_exists = True
 
                 # Retrieve item reference
-                item_data = Item.objects.get(
+                account_data = Account.objects.get(
                     id=formset.cleaned_data["id"].id
                 )
             else:
-                item_exists = False
-                item_data = Item()
+                account_exists = False
+                item_data = Account()
 
             if can_delete and item_exists:
-                # Delete the retrieved item
-                item_data.delete()
+                # Delete the retrieved account
+                account_data.delete()
             else:
                 # Collect the cleaned formset data
-                date_item = formset.cleaned_data["date_item"]
-                description = formset.cleaned_data["description"]
-                amount = formset.cleaned_data["amount"]
-                gst = formset.cleaned_data["gst"]
-
+                account_number = formset.cleaned_data["account_number"]
+                name = formset.cleaned_data["name"]
+                status = formset.cleaned_data["status"]
+            
                 # Set the model data and save the instance
-                item_data.transaction = transaction_data
-                item_data.date_item = date_item
-                item_data.description = description
-                item_data.amount = amount
-                item_data.gst = gst
-
-                item_data.save()
+                account_data.institution = institution_data
+                account_data.account_number = account_number
+                account_data.name = name
+                account_data.status = status
+            
+                account_data.save()
 
     # Setup the inline formset for the Item model
-    item_inline_formset = inlineformset_factory(
-        Transaction,
-        Item,
-        form=TransactionForm,
-        fields=("date_item", "description", "amount", "gst"),
+    account_formset = inlineformset_factory(
+        Institution,
+        Account,
+        fields=("account_number", "name", "status",),
         min_num=1,
         validate_min=True,
         can_delete=True,
@@ -187,64 +184,66 @@ def institution_edit(request, institution_id):
     # If this is a POST request then process the Form data
     if request.method == "POST":
         # Get the transaction object
-        transaction_data = get_object_or_404(Transaction, id=transaction_id)
-
+        institution_data = get_object_or_404(Institution, id=institution_id)
+        
         # Create a form instance and populate it with data from the request (binding):
-        form = TransactionForm(request.POST, instance=transaction_data)
+        form = InstitutionForm(request.POST, instance=institution_data)
 
         # Check if the form is valid:
         if form.is_valid():
-            # Create a item form instance and provide it the transaction object
-            item_formset = item_inline_formset(
-                request.POST, instance=transaction_data
+            # Create a item form instance and provide it the institution object
+            formsets = account_formset(
+                request.POST, instance=institution_data
             )
 
             if item_formset.is_valid():
-                update_transaction_form(form)
-
+                update_transaction_form(form, institution_data)
+                
                 # Cycle through each item_formset and save model data
-                for formset in item_formset:
-                    update_item_formset(formset)
+                for formset in formsets:
+                    update_account_formset(formset, institution_data)
 
                 # redirect to a new URL:
-                messages.success(request, "Expense successfully edited")
+                messages.success(
+                    request, 
+                    "Institution and accounts successfully updated"
+                )
 
-                return HttpResponseRedirect(reverse("transactions_dashboard"))
+                return HttpResponseRedirect(reverse("bank_settings"))
 
     # If this is a GET (or any other method) create populated forms
     else:
         # Populate the initial transaction data
-        transaction_data = get_object_or_404(Transaction, id=transaction_id)
-        form = TransactionForm(initial={
-            "payee_payer": transaction_data.payee_payer,
-            "memo": transaction_data.memo,
-            "date_submitted": transaction_data.date_submitted
+        institution_data = get_object_or_404(Institution, id=institution_id)
+        form = InstitutionForm(initial={
+            "name": institution_data.name,
+            "address": institution_data.address,
+            "phone": institution_data.phone,
+            "fax": institution_data.fax,
         })
 
         # Create dictionary of item data
-        items = transaction_data.item_set.all()
-        initial_item_data = []
+        accounts = institution_data.account_set.all()
+        initial_account_data = []
 
-        for item in items:
-            initial_item_data.append({
-                "id": item.id,
-                "date_item": item.date_item,
-                "description": item.description,
-                "amount": item.amount,
-                "gst": item.gst
+        for account in accounts:
+            initial_account_data.append({
+                "id": account.id,
+                "account_number": account.account_number,
+                "name": account.name,
+                "status": account.status,
             })
-        print(len(items))
+
         # Populate the initial formset with the item data
-        item_inline_formset.extra = len(items) - 1
-        item_formset = item_inline_formset(initial=initial_item_data)
+        account_formset.extra = len(accounts) - 1
+        formsets = account_formset(initial=initial_account_data)
 
     return render(
         request,
-        "transactions/edit.html",
+        "bank_settings/edit.html",
         {
-            "page_name": t_type,
             "form": form,
-            "formset": item_formset,
+            "formsets": formsets,
         },
     )
 
