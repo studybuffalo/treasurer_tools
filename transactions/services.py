dc647acaff2514430aaa28655bc64a2e460426cc
studybuffalo <studybuffalo@gmail.com>
2018-01-28 13:58:08 -0700

    Moving complex validation functions into their own method; adding function to return successfully matched IDs

--- transactions/services.py
+++ transactions/services.py
@@ -18,6 +18,78 @@ class BankReconciliation(object):
 
         return json_data
 
+    
+    def __is_valid_financial_ids(self, financial_ids):
+        """Checks that provided financial_ids are valid"""
+        valid = True
+
+        for financial_id in financial_ids:
+            # Checks that financial ID exists
+            if Transaction.objects.filter(id=financial_id).exists():
+                # Performs additional testing for match/unmatch
+                if self.function_type == "match":
+                    if ReconciliationMatch.objects.filter(financial_transaction_id=financial_id).exists():
+                        valid = False
+                        self.errors["financial_id"].append(
+                            (
+                                "{} is already reconciled. "
+                                "Unmatch the transaction before reassigning it."
+                            ).format(str(Transaction.objects.get(id=financial_id)))
+                        )
+                elif self.function_type == "unmatch":
+                    if ReconciliationMatch.objects.filter(financial_transaction_id=financial_id).exists() is False:
+                        valid = False
+                        self.errors["financial_id"].append(
+                            "{} is not a matched transaction.".format(
+                                str(Transaction.objects.get(id=financial_id))
+                            )
+                        )
+            else:
+                valid = False
+                self.errors["financial_id"].append(
+                    (
+                        "{} is not a valid financial transaction ID. "
+                        "Please make a valid selection."
+                    ).format(financial_id)
+                )
+
+        return valid
+
+    def __is_valid_bank_ids(self, bank_ids):
+        """Checks that provided bank_ids are valid"""
+        valid = True
+
+        for bank_id in bank_ids:
+            # Check that the bank ID exists
+            if BankTransaction.objects.filter(id__in=bank_ids).exists():
+                if self.function_type == "match":
+                    if ReconciliationMatch.objects.filter(bank_transaction_id=bank_id).exists():
+                        valid = False
+                        self.errors["bank_id"].append(
+                            (
+                                "{} is already reconciled. "
+                                "Unmatch the transaction before reassigning it."
+                            ).format(str(BankTransaction.objects.get(id=bank_id)))
+                        )
+                elif self.function_type == "unmatch":
+                    if ReconciliationMatch.objects.filter(bank_transaction_id=bank_id).exists() is False:
+                        valid = False
+                        self.errors["bank_id"].append(
+                            "{} is not a matched transaction.".format(
+                                str(BankTransaction.objects.get(id=bank_id))
+                            )
+                        )
+            else:
+                valid = False
+                self.errors["bank_id"].append(
+                    (
+                        "{} is not a valid bank transaction ID. "
+                        "Please make a valid selection."
+                    ).format(bank_id)
+                )
+
+        return valid
+
     def is_valid(self):
         """Checks that provided transaction & banking data is valid"""
         # pylint: disable=too-many-branches
@@ -43,66 +115,17 @@ class BankReconciliation(object):
 
         # Additional validation of financial ID data
         if financial_ids:
-            for financial_id in financial_ids:
-                # Checks that financial ID exists
-                if Transaction.objects.filter(id=financial_id).exists():
-                    # Performs additional testing for match/unmatch
-                    if self.function_type == "match":
-                        if ReconciliationMatch.objects.filter(financial_transaction_id=financial_id).exists():
-                            valid = False
-                            self.errors["financial_id"].append(
-                                (
-                                    "{} is already reconciled. "
-                                    "Unmatch the transaction before reassigning it."
-                                ).format(str(Transaction.objects.get(id=financial_id)))
-                            )
-                    elif self.function_type == "unmatch":
-                        if ReconciliationMatch.objects.filter(financial_transaction_id=financial_id).exists() is False:
-                            valid = False
-                            self.errors["financial_id"].append(
-                                "{} is not a matched transaction.".format(
-                                    str(Transaction.objects.get(id=financial_id))
-                                )
-                            )
-                else:
-                    valid = False
-                    self.errors["financial_id"].append(
-                        (
-                            "{} is not a valid financial transaction ID. "
-                            "Please make a valid selection."
-                        ).format(financial_id)
-                    )
+            financial_ids_valid = self.__is_valid_financial_ids(financial_ids)
+
+            if financial_ids_valid is False:
+                valid = False
 
         # Check that each bank_id exists
         if bank_ids:
-            for bank_id in bank_ids:
-                # Check that the bank ID exists
-                if BankTransaction.objects.filter(id__in=bank_ids).exists():
-                    if self.function_type == "match":
-                        if ReconciliationMatch.objects.filter(bank_transaction_id=bank_id).exists():
-                            valid = False
-                            self.errors["bank_id"].append(
-                                (
-                                    "{} is already reconciled. "
-                                    "Unmatch the transaction before reassigning it."
-                                ).format(str(BankTransaction.objects.get(id=bank_id)))
-                            )
-                    elif self.function_type == "unmatch":
-                        if ReconciliationMatch.objects.filter(bank_transaction_id=bank_id).exists() is False:
-                            valid = False
-                            self.errors["bank_id"].append(
-                                "{} is not a matched transaction.".format(
-                                    str(BankTransaction.objects.get(id=bank_id))
-                                )
-                            )
-                else:
-                    valid = False
-                    self.errors["bank_id"].append(
-                        (
-                            "{} is not a valid bank transaction ID. "
-                            "Please make a valid selection."
-                        ).format(bank_id)
-                    )
+            bank_ids_valid = self.__is_valid_bank_ids(bank_ids)
+
+            if bank_ids_valid is False:
+                valid = False
 
         return valid
         
@@ -117,6 +140,10 @@ class BankReconciliation(object):
                     financial_transaction=Transaction.objects.get(id=financial_id),
                     bank_transaction=BankTransaction.objects.get(id=bank_id)
                 )
+
+        # Return the ids that were successfully matched
+        self.success["financial_id"] = self.json_data["financial_ids"]
+        self.success["bank_id"] = self.json_data["bank_ids"]
 
     def delete_matches(self):
         """Removes matches between provided financial and bank transactions"""
