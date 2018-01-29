1604177883f00ed864917091c6baaf90a1315262
studybuffalo <studybuffalo@gmail.com>
2018-01-28 20:12:28 -0700

    Splitting settings into individual files for production and development

--- config/settings/production.py
+++ config/settings/production.py
@@ -20,14 +20,23 @@ DEBUG = True if env("DEBUG") == "True" else False
 # Note: This key only used for development and testing.
 SECRET_KEY = env("SECRET_KEY", default='S>}{t(v|~SNz|uS%o:?8oMqJjmnR^q~00BYjc#wBr99,W.V*u;')
 
+# SITE CONFIGURATION
+# ------------------------------------------------------------------------------
+# Hosts/domain names that are valid for this site
+# See https://docs.djangoproject.com/en/dev/ref/settings/#allowed-hosts
+ALLOWED_HOSTS = env("SERVER_ADDRESS").split(" ")
+# END SITE CONFIGURATION
+
+
 # Mail settings
 # ------------------------------------------------------------------------------
-
-EMAIL_PORT = 1025
-
-EMAIL_HOST = 'localhost'
-EMAIL_BACKEND = env('DJANGO_EMAIL_BACKEND', default='django.core.mail.backends.console.EmailBackend')
-
+EMAIL_BACKEND = env('EMAIL_BACKEND')
+DEFAULT_FROM_EMAIL = env('DEFAULT_FROM_EMAIL')
+EMAIL_HOST = env('EMAIL_HOST')
+EMAIL_HOST_USER = env('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = env('EMAIL_HOST_PASSWORD')
+EMAIL_USE_TLS = True if env('EMAIL_USE_TLS') == "True" else False
+EMAIL_PORT = env('EMAIL_PORT')
 
 # CACHING
 # ------------------------------------------------------------------------------
