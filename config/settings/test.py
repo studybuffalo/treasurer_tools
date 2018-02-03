2e3665908574d97f688c1da68bb8dfe1530e11ac
studybuffalo <studybuffalo@gmail.com>
2018-02-03 12:31:01 -0700

    Fixing error in migration; adding setings for tests

---
+++ config/settings/test.py
@@ -0,0 +1,61 @@
+"""
+Local settings for Treasurer Tools project.
+
+- Runs in debug mode
+- Uses console backend for emails
+"""
+
+from .base import *
+# pylint: skip-file
+
+# DEBUG
+# ------------------------------------------------------------------------------
+# Turn debug off so tests run faster
+DEBUG = False
+TEMPLATES[0]['OPTIONS']['debug'] = False
+
+# SECRET CONFIGURATION
+# ------------------------------------------------------------------------------
+# See: https://docs.djangoproject.com/en/dev/ref/settings/#secret-key
+# Note: This key only used for development and testing.
+SECRET_KEY = env("SECRET_KEY", default='S>}{t(v|~SNz|uS%o:?8oMqJjmnR^q~00BYjc#wBr99,W.V*u;')
+
+# Mail settings
+# ------------------------------------------------------------------------------
+EMAIL_HOST = 'localhost'
+EMAIL_PORT = 1025
+
+# In-memory email backend stores messages in django.core.mail.outbox
+# for unit testing purposes
+EMAIL_BACKEND = 'django.core.mail.backends.locmem.EmailBackend'
+
+# CACHING
+# ------------------------------------------------------------------------------
+# Speed advantages of in-memory caching without having to run Memcached
+CACHES = {
+    'default': {
+        'BACKEND': 'django.core.cache.backends.locmem.LocMemCache',
+        'LOCATION': ''
+    }
+}
+
+# TESTING
+# ------------------------------------------------------------------------------
+TEST_RUNNER = 'django.test.runner.DiscoverRunner'
+
+# PASSWORD HASHING
+# ------------------------------------------------------------------------------
+# Use fast password hasher so tests run faster
+PASSWORD_HASHERS = [
+    'django.contrib.auth.hashers.MD5PasswordHasher',
+]
+
+# TEMPLATE LOADERS
+# ------------------------------------------------------------------------------
+# Keep templates in memory so tests run faster
+TEMPLATES[0]['OPTIONS']['loaders'] = [
+    ['django.template.loaders.cached.Loader', [
+        'django.template.loaders.filesystem.Loader',
+        'django.template.loaders.app_directories.Loader',
+    ], ],
+]
