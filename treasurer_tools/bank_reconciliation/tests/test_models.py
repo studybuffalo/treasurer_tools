990a1c5108fd0f80c1dd2abfe614c28024f34cae
studybuffalo <studybuffalo@gmail.com>
2018-02-03 12:53:47 -0700

    Converting tests from fixtures to the ORM

--- treasurer_tools/bank_reconciliation/tests/test_models.py
+++ treasurer_tools/bank_reconciliation/tests/test_models.py
@@ -3,21 +3,38 @@
 from django.test import TestCase
 
 from ..models import ReconciliationMatch
+from bank_transactions.models import Institution, Account, Statement, BankTransaction
 
 class ReconciliationMatchModelTest(TestCase):
     """Tests for the ReconciliationMatch model"""
-    fixtures = [
-        "bank_reconciliation/tests/fixtures/account.json",
-        "bank_reconciliation/tests/fixtures/bank_transaction.json",
-        "bank_reconciliation/tests/fixtures/country.json",
-        "bank_reconciliation/tests/fixtures/demographics.json",
-        "bank_reconciliation/tests/fixtures/institution.json",
-        "bank_reconciliation/tests/fixtures/reconciliation_match.json",
-        "bank_reconciliation/tests/fixtures/statement.json",
-        "bank_reconciliation/tests/fixtures/transaction.json",
-        "bank_reconciliation/tests/fixtures/item.json",
-    ]
-    
+
+    def setUp(self):
+        institution_reference, _ = Institution.objects.get_or_create(
+            name="Test Institution",
+            address="1234 Bank Street",
+            phone="777-888-9999",
+            fax="999-888-7777",
+        )
+        account_reference, _ = Account.objects.get_or_create(
+            institution=institution_reference,
+            account_number="123456789",
+            name="Chequing Account",
+            status="a"
+        )
+        statement_reference, _ = Statement.objects.get_or_create(
+            account=account_reference,
+            date_start="2017-01-01",
+            date_end="2017-01-31"
+        )
+        BankTransaction.objects.get_or_create(
+            statement=statement_reference,
+            date_transaction="2017-01-01",
+            description_bank="CHQ#0001",
+            description_user="Cheque #0001",
+            amount_debit=100.00,
+            amount_credit=0.00,
+        )
+
     def test_string_representation(self):
         """Tests ReconciliationMatch string representation"""
         # pylint: disable=no-member
