fa5a99bb6823cfb91c3a4a43855aba6a7ce7140a
studybuffalo <studybuffalo@gmail.com>
2018-02-16 14:15:11 -0700

    Adding tests for financial code models

--- treasurer_tools/financial_codes/tests/test_models.py
+++ treasurer_tools/financial_codes/tests/test_models.py
@@ -3,182 +3,200 @@
 from django.test import TestCase
 
 from financial_codes.models import (
-    FinancialCodeSystem, FinancialCodeGroup, BudgetYear, FinancialCode
+    FinancialCodeSystem, BudgetYear, FinancialCodeGroup, FinancialCode
+)
+
+from .utils import (
+    create_financial_code_systems, create_budget_year, create_financial_code_groups,
+    create_financial_codes
 )
 
 class FinancialCodeSystemModelTest(TestCase):
     """Test functions for the Financial Code System model"""
-    # pylint: disable=no-member,protected-access
-    
-    fixtures = [
-        "financial_codes/tests/fixtures/financial_code_system.json",
-    ]
 
-    def test_labels(self):
+    def setUp(self):
+        self.systems = create_financial_code_systems()
+
+    def test_financial_code_system_labels(self):
         """tests a series of fields for proper label generation"""
-        test_list = [
-            {"field_name": "title", "label_name": "title"},
-            {"field_name": "date_start", "label_name": "start date"},
-            {"field_name": "date_end", "label_name": "end date"},
-        ]
 
-        for test_item in test_list:
-            financial_code_system = FinancialCodeSystem.objects.get(id=1)
-            field_label = financial_code_system._meta.get_field(test_item["field_name"]).verbose_name
-            self.assertEqual(field_label, test_item["label_name"])
+        # Test the title label
+        self.assertEqual(
+            self.systems[0]._meta.get_field("title").verbose_name,
+            "title"
+        )
+
+        # Test the date_start label
+        self.assertEqual(
+            self.systems[0]._meta.get_field("date_start").verbose_name,
+            "start date"
+        )
+
+        # Test the date_end label
+        self.assertEqual(
+            self.systems[0]._meta.get_field("date_end").verbose_name,
+            "end date"
+        )
 
     def test_title_max_length(self):
         """Tests a series of fields for proper max length"""
-        financial_code_system = FinancialCodeSystem.objects.get(id=1)
-        max_length = financial_code_system._meta.get_field("title").max_length
-        self.assertEqual(max_length, 100)
 
-    def test_string_representation(self):
+        self.assertEqual(self.systems[0]._meta.get_field("title").max_length, 100)
+
+    def test_string_representation_without_end_date(self):
         """Tests that the model string representaton returns as expected"""
         # Test entry without end date
         self.assertEqual(
-            str(FinancialCodeSystem.objects.get(id=1)),
-            "CSHP National (2014-04-01 to Present)"
+            str(self.systems[0]),
+            "National (2010-01-01 to Present)"
         )
 
+    def test_string_representation_with_end_date(self):
         # Test entry with end date
         self.assertEqual(
-            str(FinancialCodeSystem.objects.get(id=2)),
-            "CSHP Alberta Branch (2000-01-01 to 2018-03-31)"
+            str(self.systems[1]),
+            "Regional (2000-01-01 to 2015-12-31)"
         )
 
 class BudgetYearModelTest(TestCase):
     """Test functions for the BudgetYear model"""
-    # pylint: disable=no-member,protected-access
-    
-    fixtures = [
-        "financial_codes/tests/fixtures/financial_code_system.json",
-        "financial_codes/tests/fixtures/budget_year.json",
-    ]
-    
+
+    def setUp(self):
+        self.budget_year = create_budget_year()
+
     def test_financial_code_system_label(self):
         """Tests for a proper date_start label"""
-        budget_year = BudgetYear.objects.get(id=1)
-        field_label = budget_year._meta.get_field("financial_code_system").verbose_name
 
-        self.assertEqual(field_label, "financial code system")
-        
+        self.assertEqual(
+            self.budget_year._meta.get_field("financial_code_system").verbose_name,
+            "financial code system"
+        )
+
     def test_date_start_label(self):
         """Tests for a proper date_start label"""
-        budget_year = BudgetYear.objects.get(id=1)
-        field_label = budget_year._meta.get_field("date_start").verbose_name
 
-        self.assertEqual(field_label, "start date")
-        
+        self.assertEqual(
+            self.budget_year._meta.get_field("date_start").verbose_name,
+            "start date"
+        )
+
     def test_date_end_label(self):
         """Tests for a proper date_end label"""
-        budget_year = BudgetYear.objects.get(id=1)
-        field_label = budget_year._meta.get_field("date_end").verbose_name
-
-        self.assertEqual(field_label, "end date")
+        
+        self.assertEqual(
+            self.budget_year._meta.get_field("date_end").verbose_name,
+            "end date"
+        )
 
     def test_string_representation(self):
         """Tests that the model string representaton returns as expected"""
-        budget_year = BudgetYear.objects.get(id=1)
-        
-        self.assertEqual(
-            str(budget_year),
-            "{} to {}".format(budget_year.date_start, budget_year.date_end)
-        )
-        
+
+        self.assertEqual(str(self.budget_year), "2017-01-01 to 2017-12-31")
+
 class FinancialCodeGroupModelTest(TestCase):
     """Test functions for the FinancialCodeGroup"""
-    # pylint: disable=no-member,protected-access
-    
-    fixtures = [
-        "financial_codes/tests/fixtures/financial_code_system.json",
-        "financial_codes/tests/fixtures/budget_year.json",
-        "financial_codes/tests/fixtures/financial_code_group.json",
-    ]
+
+    def setUp(self):
+        self.groups = create_financial_code_groups()
 
     def test_labels(self):
         """tests a series of fields for proper label generation"""
-        test_list = [
-            {"field_name": "budget_year", "label_name": "budget year"},
-            {"field_name": "title", "label_name": "title"},
-            {"field_name": "description", "label_name": "description"},
-            {"field_name": "type", "label_name": "type"},
-            {"field_name": "status", "label_name": "status"},
-        ]
 
-        for test_item in test_list:
-            financial_code_group = FinancialCodeGroup.objects.get(id=1)
-            field_label = financial_code_group._meta.get_field(test_item["field_name"]).verbose_name
-            self.assertEqual(field_label, test_item["label_name"])
+        # Test the budget_year label
+        self.assertEqual(
+            self.groups[0]._meta.get_field("budget_year").verbose_name,
+            "budget year"
+        )
+
+        # Test the title label
+        self.assertEqual(
+            self.groups[0]._meta.get_field("title").verbose_name,
+            "title"
+        )
+
+        # Test the description label
+        self.assertEqual(
+            self.groups[0]._meta.get_field("description").verbose_name,
+            "description"
+        )
+
+        # Test the budget_year label
+        self.assertEqual(
+            self.groups[0]._meta.get_field("type").verbose_name,
+            "type"
+        )
+
+        # Test the budget_year label
+        self.assertEqual(
+            self.groups[0]._meta.get_field("status").verbose_name,
+            "status"
+        )
 
     def test_title_max_length(self):
         """Tests the title field for proper max length"""
-        financial_code_group = FinancialCodeGroup.objects.get(id=1)
-        max_length = financial_code_group._meta.get_field("title").max_length
-        self.assertEqual(max_length, 100)
-        
+
+        self.assertEqual(
+            self.groups[0]._meta.get_field("title").max_length,
+            100
+        )
+
     def test_description_max_length(self):
         """Tests a the description field for proper max length"""
-        financial_code_group = FinancialCodeGroup.objects.get(id=1)
-        max_length = financial_code_group._meta.get_field("description").max_length
-        self.assertEqual(max_length, 500)
 
-    def test_string_representation(self):
+        self.assertEqual(
+            self.groups[0]._meta.get_field("description").max_length,
+            500
+        )
+
+    def test_string_representation_expense(self):
         """Tests that the model string representaton returns as expected"""
-        financial_code_group = FinancialCodeGroup.objects.all()
 
-        for group in financial_code_group:
-            if group.type == "e":
-                test_string = "Expense - {}".format(group.title)
-            elif group.type == "r":
-                test_string = "Revenue - {}".format(group.title)
+        self.assertEqual(str(self.groups[0]), "Expense - Awards & Grants")
 
-            self.assertEqual(str(group), test_string)
+    def test_string_representation_revenue(self):
+        """Tests that the model string representaton returns as expected"""
+
+        self.assertEqual(str(self.groups[1]), "Revenue - Awards & Grants")
+
 
 class FinancialCodeModelTest(TestCase):
     """Test functions for the FinancialCode model"""
-    # pylint: disable=no-member,protected-access
-    
-    fixtures = [
-        "financial_codes/tests/fixtures/financial_code_system.json",
-        "financial_codes/tests/fixtures/financial_code_group.json",
-        "financial_codes/tests/fixtures/budget_year.json",
-        "financial_codes/tests/fixtures/financial_code.json",
-    ]
-    
+
+    def setUp(self):
+        self.codes = create_financial_codes()
+
     def test_labels(self):
         """tests a series of fields for proper label generation"""
-        test_list = [
-            {"field_name": "financial_code_group", "label_name": "financial code group"},
-            {"field_name": "code", "label_name": "code"},
-            {"field_name": "description", "label_name": "description"},
-        ]
 
-        for test_item in test_list:
-            financial_code = FinancialCode.objects.get(id=1)
-            field_label = financial_code._meta.get_field(test_item["field_name"]).verbose_name
+        # Test the financial_code_group label
+        self.assertEqual(
+            self.codes[0]._meta.get_field("financial_code_group").verbose_name,
+            "financial code group"
+        )
 
-            self.assertEqual(field_label, test_item["label_name"])
-            
+        # Test the code label
+        self.assertEqual(
+            self.codes[0]._meta.get_field("code").verbose_name,
+            "code"
+        )
+
+        # Test the description label
+        self.assertEqual(
+            self.codes[0]._meta.get_field("description").verbose_name,
+            "description"
+        )
+
     def test_code_max_length(self):
         """Tests the code field for a proper max length"""
-        financial_code = FinancialCode.objects.get(id=1)
-        max_length = financial_code._meta.get_field("code").max_length
 
-        self.assertEqual(max_length, 6)
+        self.assertEqual(self.codes[0]._meta.get_field("code").max_length, 6)
 
     def test_description_max_length(self):
         """Tests the description field for a proper max length"""
-        financial_code = FinancialCode.objects.get(id=1)
-        max_length = financial_code._meta.get_field("description").max_length
 
-        self.assertEqual(max_length, 100)
-        
+        self.assertEqual(self.codes[0]._meta.get_field("description").max_length, 100)
+
     def test_string_representation(self):
         """Tests that the model string representaton returns as expected"""
-        financial_code = FinancialCode.objects.get(id=1)
-        
-        self.assertEqual(
-            str(financial_code),
-            "{} - {}".format(financial_code.code, financial_code.description)
-        )
+
+        self.assertEqual(str(self.codes[0]), "1000 - Travel Grant")
---
+++ treasurer_tools/financial_codes/tests/utils.py
@@ -0,0 +1,86 @@
+"""Functions to assist with unit and integration testing"""
+
+from django.contrib.auth import get_user_model
+
+from financial_codes.models import (
+    FinancialCodeSystem, BudgetYear, FinancialCodeGroup, FinancialCode
+)
+
+def create_user():
+    # Create regular user
+    regular_user = get_user_model().objects.create(
+        username="user",
+        email="user@email.com"
+    )
+    regular_user.set_password("abcd123456")
+    regular_user.is_superuser = False
+    regular_user.is_staff = False
+    regular_user.is_active = True
+    regular_user.save()
+
+def create_financial_code_systems():
+    system_references = [
+        FinancialCodeSystem.objects.create(
+            title="National",
+            date_start="2010-01-01",
+            date_end=None,
+        ),
+        FinancialCodeSystem.objects.create(
+            title="Regional",
+            date_start="2000-01-01",
+            date_end="2015-12-31",
+        ),
+    ]
+
+    return system_references
+
+def create_budget_year():
+    systems = create_financial_code_systems()
+
+    year_reference = BudgetYear.objects.create(
+        financial_code_system=systems[0],
+        date_start="2017-01-01",
+        date_end="2017-12-31",
+    )
+
+    return year_reference
+
+def create_financial_code_groups():
+    year = create_budget_year()
+
+    group_references = [
+        FinancialCodeGroup.objects.create(
+            budget_year=year,
+            title="Awards & Grants",
+            description="Expenses for awards & grants",
+            type="e",
+            status="a",
+        ),
+        FinancialCodeGroup.objects.create(
+            budget_year=year,
+            title="Awards & Grants",
+            description="Revenue for awards & grants",
+            type="r",
+            status="a",
+        )
+    ]
+
+    return group_references
+
+def create_financial_codes():
+    groups = create_financial_code_groups()
+
+    code_references = [
+        FinancialCode.objects.create(
+            financial_code_group=groups[0],
+            code="1000",
+            description="Travel Grant"
+        ),
+        FinancialCode.objects.create(
+            financial_code_group=groups[1],
+            code="6000",
+            description="Travel Grant Sponsorship"
+        ),
+    ]
+
+    return code_references
