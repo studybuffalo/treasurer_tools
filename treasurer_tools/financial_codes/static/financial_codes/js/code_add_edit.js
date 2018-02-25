c930c9e643fb4a148b658292b904cecda668f62c
Joshua Torrance <torrancj@gmail.com>
2018-02-25 00:01:29 -0700

    Adding back the code_add_edit file that was accidently deleted

---
+++ treasurer_tools/financial_codes/static/financial_codes/js/code_add_edit.js
@@ -0,0 +1,37 @@
+function update_financial_code_group() {
+    const yearID = $("#id_budget_year").val();
+    const $groupSelect = $("#id_financial_code_group");
+    const $groupOptions = $groupSelect.children();
+
+    if (yearID) {
+        // Show code group options with data_system_id matching systemID
+        $groupOptions.each(function (index, option) {
+            let $option = $(option);
+
+            if ($option.attr("data-year_id") == yearID) {
+                $option.prop("hidden", false);
+            } else {
+                $option.prop("hidden", true);
+            }
+        });
+
+        // Enable the selects
+        $groupSelect.prop("disabled", false);
+    } else {
+        // No system selected - disable group and year selects
+        $groupSelect.prop("disabled", true);
+    }
+}
+
+function reset_group_selection() {
+    $("#id_financial_code_group").val("");
+}
+
+$(document).ready(function () {
+    $("#id_budget_year").on("change", function () {
+        reset_group_selection();
+        update_financial_code_group();
+    });
+
+    update_financial_code_group();
+});
