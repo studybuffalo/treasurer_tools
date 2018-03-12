60035cc6dac2f4d72e4d6ff8dbb0ffee74380ce0
studybuffalo <studybuffalo@gmail.com>
2018-03-12 16:51:56 -0600

    Correcting env type (DJANGO_SECRET_KEY > SECRET_KEY)

--- config/settings/production.py
+++ config/settings/production.py
@@ -21,7 +21,7 @@ from .base import *  # noqa
 # ------------------------------------------------------------------------------
 # See: https://docs.djangoproject.com/en/dev/ref/settings/#secret-key
 # Raises ImproperlyConfigured exception if DJANGO_SECRET_KEY not in os.environ
-SECRET_KEY = env('DJANGO_SECRET_KEY')
+SECRET_KEY = env('SECRET_KEY')
 
 
 # This ensures that Django will be able to detect a secure connection
