87a762872215d01973b3e8b3195c40d03e00cec6
studybuffalo <studybuffalo@gmail.com>
2018-03-12 22:05:29 -0600

    Commenting out unused sentry middleware import

--- config/wsgi.py
+++ config/wsgi.py
@@ -24,8 +24,8 @@ app_path = os.path.abspath(os.path.join(
     os.path.dirname(os.path.abspath(__file__)), os.pardir))
 sys.path.append(os.path.join(app_path, 'treasurer_tools'))
 
-if os.environ.get('DJANGO_SETTINGS_MODULE') == 'config.settings.production':
-    from raven.contrib.django.raven_compat.middleware.wsgi import Sentry
+#if os.environ.get('DJANGO_SETTINGS_MODULE') == 'config.settings.production':
+#    from raven.contrib.django.raven_compat.middleware.wsgi import Sentry
 
 # We defer to a DJANGO_SETTINGS_MODULE already in the environment. This breaks
 # if running multiple sites in the same mod_wsgi process. To fix this, use
