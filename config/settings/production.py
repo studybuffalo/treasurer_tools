c7ffb8d2719598b9b1b1b426dd3e8b2dc2758c2f
studybuffalo <studybuffalo@gmail.com>
2018-04-05 06:58:31 -0600

    Adding sentry to production settings

--- config/settings/production.py
+++ config/settings/production.py
@@ -30,14 +30,14 @@ SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
 # Raven sentry client
 # See https://docs.sentry.io/clients/python/integrations/django/
-#INSTALLED_APPS += ['raven.contrib.django.raven_compat', ]
+INSTALLED_APPS += ['raven.contrib.django.raven_compat', ]
 
 # Use Whitenoise to serve static files
 # See: https://whitenoise.readthedocs.io/
 WHITENOISE_MIDDLEWARE = ['whitenoise.middleware.WhiteNoiseMiddleware', ]
 MIDDLEWARE = WHITENOISE_MIDDLEWARE + MIDDLEWARE
-#RAVEN_MIDDLEWARE = ['raven.contrib.django.raven_compat.middleware.SentryResponseErrorIdMiddleware']
-#MIDDLEWARE = RAVEN_MIDDLEWARE + MIDDLEWARE
+RAVEN_MIDDLEWARE = ['raven.contrib.django.raven_compat.middleware.SentryResponseErrorIdMiddleware']
+MIDDLEWARE = RAVEN_MIDDLEWARE + MIDDLEWARE
 
 # SECURITY CONFIGURATION
 # ------------------------------------------------------------------------------
@@ -161,60 +161,58 @@ CACHES = {
 
 
 # Sentry Configuration
-#SENTRY_DSN = env('DJANGO_SENTRY_DSN')
-#SENTRY_CLIENT = env('DJANGO_SENTRY_CLIENT', default='raven.contrib.django.raven_compat.DjangoClient')
-#LOGGING = {
-#    'version': 1,
-#    'disable_existing_loggers': True,
-#    'root': {
-#        'level': 'WARNING',
-#        'handlers': ['sentry', ],
-#    },
-#    'formatters': {
-#        'verbose': {
-#            'format': '%(levelname)s %(asctime)s %(module)s '
-#                      '%(process)d %(thread)d %(message)s'
-#        },
-#    },
-#    'handlers': {
-#        'sentry': {
-#            'level': 'ERROR',
-#            'class': 'raven.contrib.django.raven_compat.handlers.SentryHandler',
-#        },
-#        'console': {
-#            'level': 'DEBUG',
-#            'class': 'logging.StreamHandler',
-#            'formatter': 'verbose'
-#        }
-#    },
-#    'loggers': {
-#        'django.db.backends': {
-#            'level': 'ERROR',
-#            'handlers': ['console', ],
-#            'propagate': False,
-#        },
-#        'raven': {
-#            'level': 'DEBUG',
-#            'handlers': ['console', ],
-#            'propagate': False,
-#        },
-#        'sentry.errors': {
-#            'level': 'DEBUG',
-#            'handlers': ['console', ],
-#            'propagate': False,
-#        },
-#        'django.security.DisallowedHost': {
-#            'level': 'ERROR',
-#            'handlers': ['console', 'sentry', ],
-#            'propagate': False,
-#        },
-#    },
-#}
-#SENTRY_CELERY_LOGLEVEL = env.int('DJANGO_SENTRY_LOG_LEVEL', logging.INFO)
-#RAVEN_CONFIG = {
-#    'CELERY_LOGLEVEL': env.int('DJANGO_SENTRY_LOG_LEVEL', logging.INFO),
-#    'DSN': SENTRY_DSN
-#}
+SENTRY_DSN = env('DJANGO_SENTRY_DSN')
+SENTRY_CLIENT = env('DJANGO_SENTRY_CLIENT', default='raven.contrib.django.raven_compat.DjangoClient')
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': True,
+    'root': {
+        'level': 'WARNING',
+        'handlers': ['sentry', ],
+    },
+    'formatters': {
+        'verbose': {
+            'format': '%(levelname)s %(asctime)s %(module)s '
+                      '%(process)d %(thread)d %(message)s'
+        },
+    },
+    'handlers': {
+        'sentry': {
+            'level': 'ERROR',
+            'class': 'raven.contrib.django.raven_compat.handlers.SentryHandler',
+        },
+        'console': {
+            'level': 'DEBUG',
+            'class': 'logging.StreamHandler',
+            'formatter': 'verbose'
+        }
+    },
+    'loggers': {
+        'django.db.backends': {
+            'level': 'ERROR',
+            'handlers': ['console', ],
+            'propagate': False,
+        },
+        'raven': {
+            'level': 'DEBUG',
+            'handlers': ['console', ],
+            'propagate': False,
+        },
+        'sentry.errors': {
+            'level': 'DEBUG',
+            'handlers': ['console', ],
+            'propagate': False,
+        },
+        'django.security.DisallowedHost': {
+            'level': 'ERROR',
+            'handlers': ['console', 'sentry', ],
+            'propagate': False,
+        },
+    },
+}
+RAVEN_CONFIG = {
+    'DSN': SENTRY_DSN
+}
 
 # Custom Admin URL, use {% url 'admin:index' %}
 ADMIN_URL = env("DJANGO_ADMIN_URL", default="admin")
