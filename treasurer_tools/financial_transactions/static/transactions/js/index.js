a83028b8b9a5a0bcf75d20b3caf417953b9c015f
studybuffalo <studybuffalo@gmail.com>
2018-07-01 21:10:43 -0600

    Reworking script to align with airbnb style guide

--- treasurer_tools/financial_transactions/static/transactions/js/index.js
+++ treasurer_tools/financial_transactions/static/transactions/js/index.js
@@ -1,218 +1,223 @@
 function convertDateToString(date) {
-    var year = date.getFullYear();
+  const year = date.getFullYear();
 
-    var month
+  let month;
 
-    if (date.getMonth() < 9) {
-        month = `0${date.getMonth() + 1}`;
-    } else {
-        month = date.getMonth() + 1;
-    }
+  if (date.getMonth() < 9) {
+    month = `0${date.getMonth() + 1}`;
+  } else {
+    month = date.getMonth() + 1;
+  }
 
-    var day;
+  let day;
 
-    if (date.getDate() < 10) {
-        day = `0${date.getDate()}`;
-    } else {
-        day = date.getDate();
-    }
+  if (date.getDate() < 10) {
+    day = `0${date.getDate()}`;
+  } else {
+    day = date.getDate();
+  }
 
-    return `${year}-${month}-${day}`;
+  return `${year}-${month}-${day}`;
 }
 
 function setDefaultDates() {
-    var today = new Date()
-    var dateEnd = convertDateToString(today);
-    var dateStart = convertDateToString(
-        new Date(today.setDate(today.getDate() - 365))
-    );
+  const today = new Date();
+  const dateEnd = convertDateToString(today);
+  const dateStart = convertDateToString(
+    new Date(today.setDate(today.getDate() - 365)),
+  );
 
-    $("#date-end").val(dateEnd);
-    $("#date-start").val(dateStart);
+  $('#date-end').val(dateEnd);
+  $('#date-start').val(dateStart);
 }
 
 function toggleDateInputs() {
-    if ($("#transaction-dates").val() === "range") {
-        $(".date").removeClass("hide");
-    } else {
-        $(".date").addClass("hide");
-    }
+  if ($('#transaction-dates').val() === 'range') {
+    $('.date').removeClass('hide');
+  } else {
+    $('.date').addClass('hide');
+  }
 }
 
 function retrieveTransactions() {
-    // Get the transaction type
-    var transactionType = $("#transaction-type").val();
+  // Get the transaction type
+  const transactionType = $('#transaction-type').val();
 
-    // Get the start and stop dates
-    var dateType = $("#transaction-dates").val();
-    var today = new Date();
-    var dateStart = "";
-    var dateEnd = "";
+  // Get the start and stop dates
+  const dateType = $('#transaction-dates').val();
+  const today = new Date();
+  let dateStart = '';
+  let dateEnd = '';
 
-    if (dateType === "30 days") {
-        dateEnd = convertDateToString(
-            new Date(today.setDate(today.getDate() + 1))
-        );
-        dateStart = convertDateToString(
-            new Date(today.setDate(today.getDate() - 30))
-        );
-    } else if (dateType === "90 days") {
-        dateEnd = convertDateToString(
-            new Date(today.setDate(today.getDate() + 1))
-        );
-        dateStart = convertDateToString(
-            new Date(today.setDate(today.getDate() - 90))
-        );
-    } else if (dateType === "1 year") {
-        dateEnd = convertDateToString(
-            new Date(today.setDate(today.getDate() + 1))
-        );
-        dateStart = convertDateToString(
-            new Date(today.setDate(today.getDate() - 365))
-        );
-    } else if (dateType === "2 years") {
-        dateEnd = convertDateToString(
-            new Date(today.setDate(today.getDate() + 1))
-        );
-        dateStart = convertDateToString(
-            new Date(today.setDate(today.getDate() - 730))
-        );
-    } else {
-        dateStart = $("#date-start").val() ? $("#date-start").val() : "";
-        dateEnd = $("#date-end").val() ? $("#date-end").val() : "";
-    }
+  if (dateType === '30 days') {
+    dateEnd = convertDateToString(
+      new Date(today.setDate(today.getDate() + 1)),
+    );
+    dateStart = convertDateToString(
+      new Date(today.setDate(today.getDate() - 30)),
+    );
+  } else if (dateType === '90 days') {
+    dateEnd = convertDateToString(
+      new Date(today.setDate(today.getDate() + 1)),
+    );
+    dateStart = convertDateToString(
+      new Date(today.setDate(today.getDate() - 90)),
+    );
+  } else if (dateType === '1 year') {
+    dateEnd = convertDateToString(
+      new Date(today.setDate(today.getDate() + 1)),
+    );
+    dateStart = convertDateToString(
+      new Date(today.setDate(today.getDate() - 365)),
+    );
+  } else if (dateType === '2 years') {
+    dateEnd = convertDateToString(
+      new Date(today.setDate(today.getDate() + 1)),
+    );
+    dateStart = convertDateToString(
+      new Date(today.setDate(today.getDate() - 730)),
+    );
+  } else {
+    dateStart = $('#date-start').val() ? $('#date-start').val() : '';
+    dateEnd = $('#date-end').val() ? $('#date-end').val() : '';
+  }
 
-    var url = "retrieve-transactions/";
-    var parameters = "?"
-        + "transaction_type=" + encodeURIComponent(transactionType)
-        + "&date_start=" + dateStart
-        + "&date_end=" + dateEnd;
+  const url = 'retrieve-transactions/';
+  const parameters = `?
+      transaction_type=${encodeURIComponent(transactionType)}
+      &date_start=${dateStart}
+      &date_end=${dateEnd}`;
 
-    $("#transactions").load(url + parameters, function () {
-        // Callback function goes here (e.g. error handling)
-    });
+  $('#transactions').load(url + parameters, () => {
+    // Callback function goes here (e.g. error handling)
+  });
 }
 
 function filterResults() {
-    var filterText = $("#text-filter").val().toUpperCase();
+  const filterText = $('#text-filter').val().toUpperCase();
 
-    // Hide all transactions
-    var $transactions = $(".transaction");
+  // Hide all transactions
+  const $transactions = $('.transaction');
 
-    $transactions.addClass("hide");
+  $transactions.addClass('hide');
 
-    $transactions.each(function (transactionIndex, transaction) {
-        var $transaction = $(transaction);
+  $transactions.each((transactionIndex, transaction) => {
+    const $transaction = $(transaction);
 
-        // Check transaction name
-        var transactionMemo = $transaction.attr("data-memo").toUpperCase();
+    // Check transaction name
+    const transactionMemo = $transaction.attr('data-memo').toUpperCase();
 
-        if (transactionMemo.indexOf(filterText) !== -1) {
-            $transaction.removeClass("hide");
-            return true;
+    if (transactionMemo.indexOf(filterText) !== -1) {
+      $transaction.removeClass('hide');
+      return false;
+    }
+
+    // Check payee/payers
+    const payeePayer = $transaction.attr('data-payee-payer').toUpperCase();
+
+    if (payeePayer.indexOf(filterText) !== -1) {
+      $transaction.removeClass('hide');
+      return false;
+    }
+
+    // Check transaction total
+    const transactionTotal = $transaction.attr('data-transaction-total').toUpperCase();
+
+    if (transactionTotal.indexOf(filterText) !== -1) {
+      $transaction.removeClass('hide');
+      return false;
+    }
+
+    $transaction.find('.item').each((itemIndex, item) => {
+      const $item = $(item);
+
+      // Check item names
+      const itemDescription = $item.attr('data-description').toUpperCase();
+
+      if (itemDescription.indexOf(filterText) !== -1) {
+        $transaction.removeClass('hide');
+        return false;
+      }
+
+      // Check item amounts
+      const itemAmount = $item.attr('data-amount').toUpperCase();
+
+      if (itemAmount.indexOf(filterText) !== -1) {
+        $transaction.removeClass('hide');
+        return false;
+      }
+
+      // Check item GST
+      const itemGST = $item.attr('data-gst').toUpperCase();
+
+      if (itemGST.indexOf(filterText) !== -1) {
+        $transaction.removeClass('hide');
+        return false;
+      }
+
+      // Check item total
+      const itemTotal = $item.attr('data-total').toUpperCase();
+
+      if (itemTotal.indexOf(filterText) !== -1) {
+        $transaction.removeClass('hide');
+        return false;
+      }
+
+      // Check item accounting codes
+      $item.find('.financial-code').each((codeIndex, code) => {
+        const $code = $(code);
+
+        // Check the accounting code
+        const codeCode = $code.attr('data-code').toUpperCase();
+
+        if (codeCode.indexOf(filterText) !== -1) {
+          $transaction.removeClass('hide');
+          return false;
         }
 
-        // Check payee/payers
-        var payeePayer = $transaction.attr("data-payee-payer").toUpperCase();
+        // Check the accounting code description
 
-        if (payeePayer.indexOf(filterText) !== -1) {
-            $transaction.removeClass("hide");
-            return true;
+        // Check the accounting code
+        const codeDescription = $code.attr('data-code-description').toUpperCase();
+
+        if (codeDescription.indexOf(filterText) !== -1) {
+          $transaction.removeClass('hide');
+          return false;
         }
 
-        // Check transaction total
-        let transactionTotal = $transaction.attr("data-transaction-total").toUpperCase();
+        return true;
+      });
 
-        if (transactionTotal.indexOf(filterText) !== -1) {
-            $transaction.removeClass("hide");
-            return true;
-        }
-
-        $transaction.find(".item").each(function (itemIndex, item) {
-            let $item = $(item);
-
-            // Check item names
-            let itemDescription = $item.attr("data-description").toUpperCase();
-
-            if (itemDescription.indexOf(filterText) !== -1) {
-                $transaction.removeClass("hide");
-                return false;
-            }
-
-            // Check item amounts
-            let itemAmount = $item.attr("data-amount").toUpperCase();
-
-            if (itemAmount.indexOf(filterText) !== -1) {
-                $transaction.removeClass("hide");
-                return false;
-            }
-
-            // Check item GST
-            let itemGST = $item.attr("data-gst").toUpperCase();
-
-            if (itemGST.indexOf(filterText) !== -1) {
-                $transaction.removeClass("hide");
-                return false;
-            }
-
-            // Check item total
-            let itemTotal = $item.attr("data-total").toUpperCase();
-
-            if (itemTotal.indexOf(filterText) !== -1) {
-                $transaction.removeClass("hide");
-                return false;
-            }
-
-            // Check item accounting codes
-            $item.find(".financial-code").each(function (codeIndex, code) {
-                let $code = $(code);
-
-                // Check the accounting code
-                let codeCode = $code.attr("data-code").toUpperCase();
-
-                if (codeCode.indexOf(filterText) !== -1) {
-                    $transaction.removeClass("hide");
-                    return false;
-                }
-
-                // Check the accounting code description
-
-                // Check the accounting code
-                let codeDescription = $code.attr("data-code-description").toUpperCase();
-
-                if (codeDescription.indexOf(filterText) !== -1) {
-                    $transaction.removeClass("hide");
-                    return false;
-                }
-            });
-        });
+      return true;
     });
+
+    return true;
+  });
 }
 
-$(document).ready(function () {
-
-    $("#transaction-type").on("change", function () {
-        retrieveTransactions();
-    });
-
-    $("#transaction-dates").on("change", function () {
-        toggleDateInputs();
-        retrieveTransactions();
-    });
-
-    $("#date-start").on("change", function () {
-        retrieveTransactions();
-    });
-
-    $("#date-end").on("change", function () {
-        retrieveTransactions();
-    });
-
-    $("#text-filter").on("keyup", function () {
-        filterResults();
-    });
-
-    toggleDateInputs();
-    setDefaultDates();
+$(document).ready(() => {
+  $('#transaction-type').on('change', () => {
     retrieveTransactions();
+  });
+
+  $('#transaction-dates').on('change', () => {
+    toggleDateInputs();
+    retrieveTransactions();
+  });
+
+  $('#date-start').on('change', () => {
+    retrieveTransactions();
+  });
+
+  $('#date-end').on('change', () => {
+    retrieveTransactions();
+  });
+
+  $('#text-filter').on('keyup', () => {
+    filterResults();
+  });
+
+  toggleDateInputs();
+  setDefaultDates();
+  retrieveTransactions();
 });
