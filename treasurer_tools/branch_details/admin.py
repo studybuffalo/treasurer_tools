6dc5c84767c5d4f9ad9a6a03f308573c00da8727
studybuffalo <studybuffalo@gmail.com>
2018-07-07 07:00:26 -0600

    Adding Branch to admin

--- treasurer_tools/branch_details/admin.py
+++ treasurer_tools/branch_details/admin.py
@@ -1,9 +1,13 @@
 from django.contrib import admin
 
-from .models import BranchMember
+from .models import Branch, BranchMember
+
+class BranchAdmin(admin.ModelAdmin):
+    pass
 
 class BranchMemberAdmin(admin.ModelAdmin):
     pass
 
 # Register the sites
+admin.site.register(Branch, BranchAdmin)
 admin.site.register(BranchMember, BranchMemberAdmin)
