b805df6c4b7fbeb4d91c82c76a18390e5fea6403
studybuffalo <studybuffalo@gmail.com>
2018-07-08 08:12:30 -0600

    Adding initial data report for balance sheet

--- treasurer_tools/reports/static/reports/js/balance_sheet.js
+++ treasurer_tools/reports/static/reports/js/balance_sheet.js
@@ -11,22 +11,6 @@ function handleMessages(data, error = false) {
   }
 }
 
-function updateDateInputs() {
-  const predefined = $('#date-predefined').val();
-  const today = moment();
-
-  if (predefined === 'last 30 days') {
-    $('#date-end').val(today.format('YYYY-MM-DD'));
-    $('#date-start').val(today.subtract(30, 'days').format('YYYY-MM-DD'));
-  } else if (predefined === 'last year') {
-    $('#date-end').val(today.format('YYYY-MM-DD'));
-    $('#date-start').val(today.subtract(1, 'years').format('YYYY-MM-DD'));
-  } else if (predefined === 'year-to-date') {
-    $('#date-start').val(moment(`${today.year()}-01-01`).format('YYYY-MM-DD'));
-    $('#date-end').val(today.format('YYYY-MM-DD'));
-  }
-}
-
 function updateTotals() {
   // Collect all the revenue transactions
   const $revenueTransactions = $('#revenue-accounts .transaction');
@@ -74,55 +58,28 @@ function updateTotals() {
 
 function retrieveReport() {
   // Get the financial code system
-  const financialCodeSystem = $('#financial-code-system').val();
+  const budgetYear = $('#budget-year').val();
 
-  // Get the start and stop dates
-  const dateStart = $('#date-start').val() ? $('#date-start').val() : '';
-  const dateEnd = $('#date-end').val() ? $('#date-end').val() : '';
+  if (budgetYear) {
+    const url = 'retrieve-report/';
+    const parameters = `?budget_year=${budgetYear}`;
 
-  const url = 'retrieve-report/';
-  const parameters = `?financial_code_system=${financialCodeSystem}&date_start=${dateStart}&date_end=${dateEnd}`;
-
-  $('#report').load(url + parameters, (response, status, xhr) => {
-    if (status === 'success') {
-      updateTotals();
-    } else if (status === 'error') {
-      handleMessages(`${status.status} error: ${xhr.statusText}`, true);
-    } else {
-      handleMessages(`Error: ${response}`, true);
-    }
-    // Callback function goes here (e.g. error handling)
-  });
-}
-
-function updatePredefinedSelect() {
-  /*
-   * Changes the predefined select to custom when date input changed
-   */
-
-  $('#date-predefined').val('custom');
+    $('#report').load(url + parameters, (response, status, xhr) => {
+      if (status === 'success') {
+        updateTotals();
+      } else if (status === 'error') {
+        handleMessages(`${status.status} error: ${xhr.statusText}`, true);
+      } else {
+        handleMessages(`Error: ${response}`, true);
+      }
+    });
+  }
 }
 
 $(document).ready(() => {
-  $('#financial-code-system').on('change', () => {
+  $('#budget-year').on('change', () => {
     retrieveReport();
   });
 
-  $('#date-predefined').on('change', () => {
-    updateDateInputs();
-    retrieveReport();
-  });
-
-  $('#date-start').on('change', () => {
-    updatePredefinedSelect();
-    retrieveReport();
-  });
-
-  $('#date-end').on('change', () => {
-    updatePredefinedSelect();
-    retrieveReport();
-  });
-
-  updateDateInputs();
   retrieveReport();
 });
--- treasurer_tools/reports/views.py
+++ treasurer_tools/reports/views.py
@@ -2,9 +2,13 @@
 
 from django.contrib.auth.decorators import login_required
 from django.db.models import Q, Sum
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
-from financial_codes.models import FinancialCodeSystem, FinancialCode
+from bank_reconciliation.models import ReconciliationMatch
+from bank_transactions.models import BankTransaction
+from financial_codes.models import FinancialCodeSystem, BudgetYear, FinancialCode
+from financial_transactions.models import FinancialTransaction
+from investments.models import Investment
 
 
 @login_required
@@ -25,9 +29,6 @@ def income_statement_dashboard(request):
         "reports/income_statement.html",
         context={
             "financial_code_systems": FinancialCodeSystem.objects.all(),
-            "date_years": [1],
-            "date_budget_years": [2],
-            "date_months": [3],
         },
     )
 
@@ -73,11 +74,100 @@ def retrieve_income_statement(request):
 @login_required
 def retrieve_balance_sheet(request):
     """Retrieves data for the balance sheet report"""
+    # Need all assets vs. All Liabilities
+    # Need on hand cash, investments, accounts payable
+    # Need any debt or liability (accounts payable)
+
+    budget_year_id = request.GET.get("budget_year", None)
+
+    if budget_year_id:
+        budget_year = get_object_or_404(BudgetYear, id=budget_year_id)
+        date_start = budget_year.date_start
+        date_end = budget_year.date_end
+
+        # CALCULATE CASH
+        # Get all the bank transactions between start and end date
+        bank_transaction_totals = BankTransaction.objects.filter(
+
+            date_transaction__lte=date_end
+        ).aggregate(
+            debit=Sum("amount_debit"), credit=Sum("amount_credit")
+        )
+        debit = bank_transaction_totals["debit"] if bank_transaction_totals["debit"] else 0
+        credit = bank_transaction_totals["credit"] if bank_transaction_totals["credit"] else 0
+        cash = credit - debit
+
+        # CALCULATE INVESTMENTS
+        # Get all investments that started before/during this budget year
+        # and haven't matured
+        investment_totals = Investment.objects.filter(
+            Q(date_invested__lte=date_end)
+            & Q(date_matured__gte=date_end)
+        ).aggregate(total=Sum("amount_matured"))
+        investments = investment_totals['total'] if investment_totals['total'] else 0
+
+        # CALCULATE ACCOUNTS RECEIVABLE
+        # Get all the reconciled transactions
+        reconciled_transactions = list(ReconciliationMatch.objects.values_list(
+            "financial_transaction__id", flat=True
+        ))
+
+        # Get all the revenue transactions that have not been reconciled
+        revenue_transactions = FinancialTransaction.objects.filter(
+            transaction_type="r"
+        ).exclude(
+            id__in=reconciled_transactions
+        )
+
+        # Cycle through each transaction and total the amount
+        accounts_receivable = 0
+
+        for transaction in revenue_transactions:
+            accounts_receivable += transaction.total
+
+        # TOTAL ASSETS
+        assets_total = cash + investments + accounts_receivable
+
+        # CALCULATE DEBT
+        # Not currently being tracked in application
+        debt = 0
+
+        # CALCULATE ACCOUNTS PAYABLE
+        # Get all the expense transactions that have not been reconciled
+        expense_transactions = FinancialTransaction.objects.filter(
+            transaction_type="e"
+        ).exclude(
+            id__in=reconciled_transactions
+        )
+
+        # Cycle through each transaction and total the amount
+        accounts_payable = 0
+
+        for transaction in expense_transactions:
+            accounts_payable += transaction.total
+
+        # TOTAL LIABILITIES
+        liabilities_total = debt + accounts_payable
+    else:
+        cash = 0
+        investments = 0
+        accounts_receivable = 0
+        assets_total = 0
+        debt = 0
+        accounts_payable = 0
+        liabilities_total = 0
 
     return render(
         request,
         "reports/balance_sheet_report.html",
         context={
+            "cash": cash,
+            "investments": investments,
+            "accounts_receivable": accounts_receivable,
+            "assets_total": assets_total,
+            "debt": debt,
+            "accounts_payable": accounts_payable,
+            "liabilities_total": liabilities_total,
         }
     )
 
@@ -89,5 +179,6 @@ def balance_sheet_dashboard(request):
         request,
         "reports/balance_sheet.html",
         context={
+            "financial_code_systems": FinancialCodeSystem.objects.all(),
         },
     )
