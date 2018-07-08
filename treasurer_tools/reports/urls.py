68543c141528263267509d2d8a93c40158e2f288
studybuffalo <studybuffalo@gmail.com>
2018-07-07 21:20:50 -0600

    Switching to path instead of url

--- treasurer_tools/reports/urls.py
+++ treasurer_tools/reports/urls.py
@@ -1,16 +1,16 @@
 """Main Site URL Conf"""
-from django.conf.urls import url
+from django.urls import path
 
 from .views import (
     dashboard, balance_sheet_dashboard, income_statement_dashboard,
     retrieve_income_statement
 )
 
-app_name="reports"
+app_name = "reports"
 
 urlpatterns = [
-    url(r"balance-sheet/$", balance_sheet_dashboard, name="balance_sheet"),
-    url(r"^income-statement/retrieve-report/", retrieve_income_statement),
-    url(r"^income-statement/$", income_statement_dashboard, name="income_statement"),
-    url(r"^$", dashboard, name="dashboard"),
+    path("balance-sheet/", balance_sheet_dashboard, name="balance_sheet"),
+    path("income-statement/retrieve-report/", retrieve_income_statement),
+    path("income-statement/", income_statement_dashboard, name="income_statement"),
+    path("", dashboard, name="dashboard"),
 ]
