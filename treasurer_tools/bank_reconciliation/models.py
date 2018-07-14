6a98ebacf8511a1bca9704731196f599ccabe8e8
studybuffalo <studybuffalo@gmail.com>
2018-07-14 15:32:47 -0600

    Removing the reconciledmatch model

--- treasurer_tools/bank_reconciliation/models.py
+++ treasurer_tools/bank_reconciliation/models.py
@@ -6,26 +6,3 @@ from simple_history.models import HistoricalRecords
 
 class ReconciliationGroup(models.Model):
     history = HistoricalRecords()
-
-class ReconciliationMatch(models.Model):
-    """Links bank transaction to one or more financial transactions"""
-    group = models.ForeignKey(
-        ReconciliationGroup,
-        on_delete=models.CASCADE,
-    )
-    bank_transaction = models.ForeignKey(
-        'bank_transactions.BankTransaction',
-        on_delete=models.CASCADE,
-        related_name="rm_bank_transaction",
-    )
-    financial_transaction = models.ForeignKey(
-        'financial_transactions.FinancialTransaction',
-        on_delete=models.CASCADE,
-        related_name="rm_financial_transaction",
-    )
-    history = HistoricalRecords()
-
-    def __str__(self):
-        return "{} - {}".format(
-            self.bank_transaction, self.financial_transaction
-        )
