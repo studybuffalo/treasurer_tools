10a2dae3c3feefbb27f49c0bfe7d60c5752c3134
studybuffalo <studybuffalo@gmail.com>
2020-10-14 17:30:46 -0600

    Adding new forms and fields to better manage users

--- treasurer_tools/users/admin.py
+++ treasurer_tools/users/admin.py
@@ -1,11 +1,50 @@
 from django.contrib import admin
 from django.contrib.auth import get_user_model
+from django.contrib.auth.admin import UserAdmin as AuthUserAdmin
+from django.contrib.auth.forms import UserChangeForm, UserCreationForm
 
+
+USER = get_user_model()
+
+
+class MyUserChangeForm(UserChangeForm):
+    """Extending user change form."""
+    class Meta(UserChangeForm.Meta):
+        model = USER
+
+
+class MyUserCreationForm(UserCreationForm):
+    """Extending user creation form."""
+    class Meta(UserCreationForm.Meta):
+        model = USER
+        fields = ('name', 'email')
+
+
+@admin.register(USER)
 class UserAdmin(admin.ModelAdmin):
-    fields = (
-        'username', 'name', 'email', 'groups', 'user_permissions',
-        'is_staff', 'is_active',
+    form = MyUserChangeForm
+    fieldsets = (
+        (
+            None,
+            {'fields': ('email', 'password')}
+        ),
+        (
+            'Personal info',
+            {'fields': ('name',)}
+        ),
+        (
+            'Permissions',
+            {'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions')}
+        ),
+        (
+            'Important dates',
+            {'fields': ('last_login', 'date_joined')}
+        ),
     )
-
-# Register the sites
-admin.site.register(get_user_model(), UserAdmin)
+    add_form = MyUserCreationForm
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('name', 'email', 'password1', 'password2'),
+        }),
+    )
