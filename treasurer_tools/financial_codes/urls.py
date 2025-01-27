3d1318bc32e700b62cb90ddab7c88cd03790a107
Study Buffalo <joshua@torrance.io>
2025-01-26 17:09:36 -0700

    Updated URLs to new path functions

--- treasurer_tools/financial_codes/urls.py
+++ treasurer_tools/financial_codes/urls.py
@@ -1,23 +1,23 @@
 """Financial Code URLs"""
-from django.conf.urls import url
+from django.urls import path
 
 from . import views
 
 app_name = "financial_codes"
 
 urlpatterns = [
-    url(r"^system/add/", views.system_add, name="system_add"),
-    url(r"^system/edit/(?P<system_id>\d+)$", views.system_edit, name="system_edit"),
-    url(r"^system/delete/(?P<system_id>\d+)$", views.system_delete, name="system_delete"),
-    url(r"^group/add/", views.group_add, name="group_add"),
-    url(r"^group/edit/(?P<group_id>\d+)$", views.group_edit, name="group_edit"),
-    url(r"^group/delete/(?P<group_id>\d+)$", views.group_delete, name="group_delete"),
-    url(r"^year/add/", views.year_add, name="year_add"),
-    url(r"^year/edit/(?P<year_id>\d+)$", views.year_edit, name="year_edit"),
-    url(r"^year/delete/(?P<year_id>\d+)$", views.year_delete, name="year_delete"),
-    url(r"^year/copy/(?P<year_id>\d+)$", views.year_copy, name="year_copy"),
-    url(r"^code/add/", views.code_add, name="code_add"),
-    url(r"^code/edit/(?P<code_id>\d+)$", views.code_edit, name="code_edit"),
-    url(r"^code/delete/(?P<code_id>\d+)$", views.code_delete, name="code_delete"),
-    url(r"^$", views.dashboard, name="dashboard"),
+    path('system/add/', views.system_add, name="system_add"),
+    path('system/edit/<int:system_id>/', views.system_edit, name="system_edit"),
+    path('system/delete/<int:system_id>/', views.system_delete, name="system_delete"),
+    path('group/add/', views.group_add, name="group_add"),
+    path('group/edit/<int:group_id>/', views.group_edit, name="group_edit"),
+    path('group/delete/<int:group_id>/', views.group_delete, name="group_delete"),
+    path('year/add/', views.year_add, name="year_add"),
+    path('year/edit/<int:year_id>/', views.year_edit, name="year_edit"),
+    path('year/delete/<int:year_id>/', views.year_delete, name="year_delete"),
+    path('year/copy/<int:year_id>/', views.year_copy, name="year_copy"),
+    path('code/add/', views.code_add, name="code_add"),
+    path('code/edit/<int:code_id>/', views.code_edit, name="code_edit"),
+    path('code/delete/<int:code_id>/', views.code_delete, name="code_delete"),
+    path('', views.dashboard, name="dashboard"),
 ]
