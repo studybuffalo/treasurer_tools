7a1133306559c115f551b7903a7e29ca4ca42ccf
Study Buffalo <joshua@torrance.io>
2025-02-08 08:41:59 -0700

    Updated STATIC_URL to current doc default

--- config/settings/base.py
+++ config/settings/base.py
@@ -221,7 +221,7 @@ TEMPLATES = [
 STATIC_ROOT = ROOT_DIR('staticfiles')
 
 # See: https://docs.djangoproject.com/en/dev/ref/settings/#static-url
-STATIC_URL = '/static/'
+STATIC_URL = 'static/'
 
 # See: https://docs.djangoproject.com/en/dev/ref/contrib/staticfiles/#std:setting-STATICFILES_DIRS
 STATICFILES_DIRS = [
