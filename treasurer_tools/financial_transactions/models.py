8ddb847707ae6f022ad271fc9b5696ec48c90f80
Study Buffalo <joshua@torrance.io>
2025-02-09 06:47:04 -0700

    Standardized to single quotes

--- treasurer_tools/financial_transactions/models.py
+++ treasurer_tools/financial_transactions/models.py
@@ -18,23 +18,23 @@ class FinancialTransaction(models.Model):
     payee_payer = models.ForeignKey(
         PayeePayer,
         help_text=(
-            "The individual, organization, or company this transaction "
-            "applies to"
+            'The individual, organization, or company this transaction '
+            'applies to'
         ),
         on_delete=models.PROTECT,
-        verbose_name="payee or payer"
+        verbose_name='payee or payer'
     )
     transaction_type = models.CharField(
         choices=(
-            ("e", "expense"),
-            ("r", "revenue"),
+            ('e', 'expense'),
+            ('r', 'revenue'),
         ),
-        default="e",
-        help_text="The type of transaction",
+        default='e',
+        help_text='The type of transaction',
         max_length=1,
     )
     memo = models.CharField(
-        help_text="Overall summary of the transaction",
+        help_text='Overall summary of the transaction',
         max_length=1000,
     )
     submitter = models.CharField(
@@ -43,7 +43,7 @@ class FinancialTransaction(models.Model):
     )
     date_submitted = models.DateField(
         default=timezone.now,
-        verbose_name="submission date"
+        verbose_name='submission date'
     )
     submission_notes = models.CharField(
         blank=True,
@@ -56,17 +56,17 @@ class FinancialTransaction(models.Model):
         on_delete=models.SET_NULL,
         blank=True,
         null=True,
-        related_name="financialtransactions",
+        related_name='financialtransactions',
     )
     history = HistoricalRecords()
 
     def __str__(self):
-        if self.transaction_type == "e":
-            return_string = "{} - Expense - {} - {}".format(
+        if self.transaction_type == 'e':
+            return_string = '{} - Expense - {} - {}'.format(
                 self.date_submitted, self.payee_payer, self.memo[:100]
             )
-        elif self.transaction_type == "r":
-            return_string = "{} - Revenue - {} - {}".format(
+        elif self.transaction_type == 'r':
+            return_string = '{} - Revenue - {} - {}'.format(
                 self.date_submitted, self.payee_payer, self.memo[:100]
             )
 
@@ -113,29 +113,29 @@ class Item(models.Model):
     transaction = models.ForeignKey(
         FinancialTransaction,
         on_delete=models.CASCADE,
-        related_name="items",
+        related_name='items',
     )
     date_item = models.DateField(
         default=timezone.now,
-        help_text="The date the item was purchased, cheque written, etc.",
-        verbose_name="date"
+        help_text='The date the item was purchased, cheque written, etc.',
+        verbose_name='date'
     )
     description = models.CharField(
-        help_text="A description of the item",
+        help_text='A description of the item',
         max_length=500,
     )
     amount = models.DecimalField(
         decimal_places=2,
         default=0,
-        help_text="The pre-tax dollar value",
+        help_text='The pre-tax dollar value',
         max_digits=12,
     )
     gst = models.DecimalField(
         decimal_places=2,
         default=0,
-        help_text="The tax (GST/HST) dollar value",
+        help_text='The tax (GST/HST) dollar value',
         max_digits=12,
-        verbose_name="GST/HST",
+        verbose_name='GST/HST',
     )
     financial_codes = models.ManyToManyField(
         FinancialCode,
@@ -144,7 +144,7 @@ class Item(models.Model):
     history = HistoricalRecords()
 
     def __str__(self):
-        return "{} - {} - ${}".format(
+        return '{} - {} - ${}'.format(
             self.date_item, self.description, self.total
         )
 
@@ -175,4 +175,4 @@ class FinancialCodeMatch(models.Model):
     history = HistoricalRecords()
 
     def __str__(self):
-        return "{} - {}".format(str(self.item), str(self.financial_code))
+        return '{} - {}'.format(str(self.item), str(self.financial_code))
